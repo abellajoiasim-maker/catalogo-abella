905be0f5c960e1a9155a8eb2a6f984ac45575275
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-04-22 19:58:43 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+<!-- ========================= -->
+<!-- /js/app.js -->
+<!-- ========================= -->
+<script>
+let carrinho = JSON.parse(localStorage.getItem('carrinho') || '[]');
 
+function salvarCarrinho(){
+ localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
+function addCarrinho(prod){
+ const item = carrinho.find(i=>i.id===prod.id);
+ if(item) item.quantidade++;
+ else carrinho.push({...prod, quantidade:1});
+ salvarCarrinho();
+ atualizarBarra();
+}
+
+function atualizarBarra(){
+ let total = 0;
+ carrinho.forEach(i=> total += i.price * i.quantidade);
+ const el = document.getElementById("totalBar");
+ if(el) el.innerText = "R$ " + total.toFixed(2);
+}
+</script>
