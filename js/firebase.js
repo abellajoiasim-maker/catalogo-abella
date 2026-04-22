b4a77054d829757a93620f00b41879f607bed775
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-04-22 19:57:49 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,13 @@
+<!-- ========================= -->
+<!-- /js/firebase.js -->
+<!-- ========================= -->
+<script>
+const firebaseConfig = {
+ apiKey: "AIzaSyDPBZSxW8XjtQmDMUknzAyIlFda51MvMJY",
+ databaseURL: "https://catalogo-abella-joias-default-rtdb.firebaseio.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+</script>
 
